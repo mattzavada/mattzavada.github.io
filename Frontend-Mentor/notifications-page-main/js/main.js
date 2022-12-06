8d187c0ca958a3769473c318c8c8ed369779977a
Matthew Zavada <118411399+mattzavada@users.noreply.github.com>
2022-12-05 22:52:13 -0500

    Starting working on JS

--- Frontend-Mentor/notifications-page-main/js/main.js
+++ Frontend-Mentor/notifications-page-main/js/main.js
@@ -1,1 +1,40 @@
 const unreadMessages = document.querySelectorAll("section[data-unread='true']");
+
+const arrayUnreadMessages = Array.from(unreadMessages);
+
+const markAllAsRead = document.getElementById("mark__all__read");
+
+markAllAsRead.addEventListener("click", (event) => {
+  unreadMessages.forEach((item) => {
+    const isUnread = item.getAttribute("data-unread");
+    if (isUnread === "true") {
+      markAsRead(item);
+    }
+  });
+});
+
+function updateUnreadTotal() {
+  document.getElementById("unread__count").textContent =
+    arrayUnreadMessages.length;
+}
+
+function markAsRead(item) {
+  item.setAttribute("data-unread", "false");
+  arrayUnreadMessages.pop();
+  updateUnreadTotal();
+}
+
+function addClickEvent() {
+  unreadMessages.forEach((item) => {
+    item.addEventListener("click", (event) => {
+      const isUnread = item.getAttribute("data-unread");
+      if (isUnread === "true") {
+        markAsRead(item);
+      }
+    });
+  });
+}
+
+addClickEvent();
+
+updateUnreadTotal();
