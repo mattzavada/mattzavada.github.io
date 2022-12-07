1c716618bbaee9602c2ed5ecb200aaff48ce9c93
Matthew Zavada <118411399+mattzavada@users.noreply.github.com>
2022-12-06 21:38:49 -0500

    Finished JS and update README

--- Frontend-Mentor/notifications-page-main/js/main.js
+++ Frontend-Mentor/notifications-page-main/js/main.js
@@ -1,40 +1,49 @@
+// Grab all section nodes with data attribute of true
 const unreadMessages = document.querySelectorAll("section[data-unread='true']");
 
+// Copy nodelist to array to track number of unread instead of searching the DOM each time to track how many notification are still unread
 const arrayUnreadMessages = Array.from(unreadMessages);
 
-const markAllAsRead = document.getElementById("mark__all__read");
+// Add onclick events to all sections and mark all as read button
 
-markAllAsRead.addEventListener("click", (event) => {
+function addClickEvents() {
   unreadMessages.forEach((item) => {
-    const isUnread = item.getAttribute("data-unread");
-    if (isUnread === "true") {
+    item.addEventListener("click", (event) => {
       markAsRead(item);
-    }
+    });
   });
-});
+  document
+    .getElementById("mark__all__read")
+    .addEventListener("click", (event) => {
+      markAllAsRead(unreadMessages);
+    });
+}
+
+// Changes data-unread attribute on node, removes item from array and updates the total
+
+function markAsRead(item) {
+  if (item.getAttribute("data-unread") === "true") {
+    item.setAttribute("data-unread", "false");
+    arrayUnreadMessages.pop();
+    updateUnreadTotal();
+  }
+}
+
+// Loop through provided nodelist, marking each as read
+
+function markAllAsRead(items) {
+  items.forEach((item) => {
+    markAsRead(item);
+  });
+}
+
+// Updates unread count element
 
 function updateUnreadTotal() {
   document.getElementById("unread__count").textContent =
     arrayUnreadMessages.length;
 }
 
-function markAsRead(item) {
-  item.setAttribute("data-unread", "false");
-  arrayUnreadMessages.pop();
-  updateUnreadTotal();
-}
-
-function addClickEvent() {
-  unreadMessages.forEach((item) => {
-    item.addEventListener("click", (event) => {
-      const isUnread = item.getAttribute("data-unread");
-      if (isUnread === "true") {
-        markAsRead(item);
-      }
-    });
-  });
-}
-
-addClickEvent();
+addClickEvents();
 
 updateUnreadTotal();
