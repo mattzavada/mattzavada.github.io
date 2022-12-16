f55c9d09076c5c8b1b8ff0c8be222d51b9213fb1
Matthew Zavada <junkzavada@gmail.com>
2022-12-16 16:59:42 -0500

    Add eventlistners to Todo app

--- Udemy/The-Modern-JavaScript-Bootcamp/todo-app/notes-app.js
+++ Udemy/The-Modern-JavaScript-Bootcamp/todo-app/notes-app.js
@@ -26,17 +26,64 @@ const todos = [
   },
 ];
 
+// Object for tracking filters
+const filters = {
+  searchText: "",
+  filterCompleted: false,
+};
+
+// Clear completed todos
 const incompleteTodos = todos.filter(function (item) {
   return !item.completed;
 });
 
+//Render todo on screen
+const renderTodos = function (array, filter) {
+  // Clear current list of todos
+  document.querySelector("#todos").innerHTML = "";
+
+  // Filter array based on filter
+  const filteredListText = array.filter(function (item) {
+    return item.text.toLowerCase().includes(filter.searchText.toLowerCase());
+  });
+
+  // const filteredListStatus = filteredListText.filter(function (item) {
+  //   return item.completed;
+  // });
+
+  // Create toto elements and append to div
+  filteredListText.forEach(function (todo) {
+    const todoText = document.createElement("p");
+    todoText.textContent = `${todo.text} - ${todo.completed}`;
+    document.querySelector("#todos").appendChild(todoText);
+  });
+};
+
+// Eventlistners
+document.querySelector("#search-query").addEventListener("input", function (e) {
+  // Set filters based on input
+  filters.searchText = e.target.value;
+
+  // Re-render list
+  renderTodos(todos, filters);
+});
+
+document.querySelector("#add-todo").addEventListener("click", function (e) {
+  console.log("Did this work?");
+});
+
+document
+  .querySelector("#clear-completed")
+  .addEventListener("click", function (e) {
+    if (e.target.checked) {
+      console.log("checked");
+    }
+  });
+
+// Initialize page
+
+renderTodos(todos, filters);
+
 const subHeading = document.createElement("h2");
 subHeading.textContent = `You have ${incompleteTodos.length} todos left to complete`;
-
 document.querySelector("h1").insertAdjacentElement("afterend", subHeading);
-
-incompleteTodos.forEach(function (item) {
-  const newTodo = document.createElement("p");
-  newTodo.textContent = item.text;
-  document.querySelector("body").appendChild(newTodo);
-});
