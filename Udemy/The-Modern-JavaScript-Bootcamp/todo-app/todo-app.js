58d52d96153dcb98d34d87583014a9dee7c564b3
Matthew Zavada <118411399+mattzavada@users.noreply.github.com>
2022-12-22 22:42:08 -0500

    Uploading final

--- Udemy/The-Modern-JavaScript-Bootcamp/todo-app/todo-app.js
+++ Udemy/The-Modern-JavaScript-Bootcamp/todo-app/todo-app.js
@@ -32,32 +32,24 @@ const filters = {
   filterCompleted: true,
 };
 
-const numNotCompleted = function (array) {
-  return array.filter((item) => !item.completed).length;
-};
-
 //Render todos on screen
 const renderTodos = function (array, filter) {
   // Clear current list of todos and sub-heading
   document.querySelector("#todos").innerHTML = "";
   document.querySelector("#left-to-complete").textContent = "";
 
-  // Initialize filtered list
-  let filteredList = array;
+  const filteredList = array.filter(function (item) {
+    const searchTextMatch = item.text
+      .toLowerCase()
+      .includes(filter.searchText.toLowerCase());
+    const hideCompletedMatch = !filter.filterCompleted || !item.completed;
 
-  // Filter array based on text filter
-  if (filter.searchText != "") {
-    filteredList = array.filter(function (item) {
-      return item.text.toLowerCase().includes(filter.searchText.toLowerCase());
-    });
-  }
+    // Return object if it matches the string search and the hide completed checkbox
+    return searchTextMatch && hideCompletedMatch;
+  });
 
-  // Filter array based on completed
-  if (filter.filterCompleted) {
-    filteredList = filteredList.filter(function (item) {
-      return !item.completed;
-    });
-  }
+  // Obtain total of unfinished todos
+  const numNotCompleted = filteredList.filter((item) => !item.completed).length;
 
   // Render todo elements and append to div
   filteredList.forEach(function (todo) {
@@ -68,7 +60,7 @@ const renderTodos = function (array, filter) {
 
   document.querySelector(
     "#left-to-complete"
-  ).textContent = `You have ${numNotCompleted(todos)} todos left to complete`;
+  ).textContent = `You have ${numNotCompleted} todos left to complete`;
 };
 
 // Eventlistners
@@ -103,14 +95,9 @@ document
 // Completed check box
 document
   .querySelector("#hide-completed")
-  .addEventListener("click", function (e) {
-    if (e.target.checked) {
-      filters.filterCompleted = true;
-      renderTodos(todos, filters);
-    } else {
-      filters.filterCompleted = false;
-      renderTodos(todos, filters);
-    }
+  .addEventListener("change", function (e) {
+    filters.filterCompleted = e.target.checked;
+    renderTodos(todos, filters);
   });
 
 // Initialize page
